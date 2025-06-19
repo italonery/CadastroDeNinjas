955f628eee73a725eb1802ff97148e26d43cb2c7
italonery <babaduquedev@gmail.com>
2025-06-19 14:05:29 -0300

    chore: include `DTO` version for `entity` and manually mapping

--- src/main/java/dev/java10x/CadastroDeNinjas/mapper/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/mapper/NinjaMapper.java
@@ -1,4 +1,33 @@
 package dev.java10x.CadastroDeNinjas.mapper;
 
-public class NInjaMapper {
+import dev.java10x.CadastroDeNinjas.dto.NinjaDTO;
+import dev.java10x.CadastroDeNinjas.model.Ninja;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NinjaMapper {
+
+    public Ninja map(NinjaDTO ninjaDTO) {
+        Ninja ninja = new Ninja();
+        ninja.setId(ninjaDTO.getId());
+        ninja.setNome(ninjaDTO.getNome());
+        ninja.setEmail(ninjaDTO.getEmail());
+        ninja.setIdade(ninjaDTO.getIdade());
+        ninja.setMissao(ninjaDTO.getMissao());
+        ninja.setRank(ninjaDTO.getRank());
+
+        return ninja;
+    }
+
+    public NinjaDTO map(Ninja ninja) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninja.getId());
+        ninjaDTO.setNome(ninja.getNome());
+        ninjaDTO.setEmail(ninja.getEmail());
+        ninjaDTO.setIdade(ninja.getIdade());
+        ninjaDTO.setMissao(ninja.getMissao());
+        ninjaDTO.setRank(ninja.getRank());
+
+        return ninjaDTO;
+    }
 }
