7a79a341569e197d5d2916741a55c1a66d15eaac
italonery <babaduquedev@gmail.com>
2025-06-19 23:32:15 -0300

    refactor: remove unnecessary get and set methods from models

--- src/main/java/dev/java10x/CadastroDeNinjas/model/Ninja.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/model/Ninja.java
@@ -38,28 +38,4 @@ public class Ninja {
     @ManyToOne
     @JoinColumn(name = "missao_id") // Join com id da missão
     private Missao missao;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
