e81d20c799b8277e3a1eaf745249cff27c2ead1f
italonery <babaduquedev@gmail.com>
2025-06-19 23:32:15 -0300

    refactor: remove unnecessary get and set methods from models

--- src/main/java/dev/java10x/CadastroDeNinjas/model/Missao.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/model/Missao.java
@@ -26,28 +26,4 @@ public class Missao {
     @OneToMany(mappedBy = "missao")
     @JsonIgnore // Ignora o loop de serialização
     private List<Ninja> ninjas;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
